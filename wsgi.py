e60b3edade9f89e02b1708dacbd27f196ecce13a
Nailik2namek <148283246+Nailik2namek@users.noreply.github.com>
2023-10-18 15:25:34 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from App.py import app  # Remplacez 'your_module' par le nom de votre module principal
+from Main.py import app  # Remplacez 'your_module' par le nom de votre module principal
 
 if __name__ == "__main__":
     app.run()
